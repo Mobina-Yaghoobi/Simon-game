301e99aaf093c06ffab64d55846f51628a6a7541
Mobina <mobinayaghoobi55@gmail.com>
2026-07-06 03:50:30 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("keypress", function(){
+document.querySelector("h1").innerHTML = "level 1";
+document.querySelector("#green").style.visibility = "hidden";
+document.querySelector("#green").style.visibility = "visible";
+
+
+});
+
