13e83a55963eb40ea9f9e66e9ed25695c553ddb6
Mobina <mobinayaghoobi55@gmail.com>
2026-07-09 02:35:27 -0700

    computer random move

--- index.js
+++ index.js
@@ -29,25 +29,76 @@ function SoundRelatedToEach(BoxColor){
 
 
 
-function ClickResponse(){
+// function ClickResponse(){
 
-    for (var i = 0; i < document.querySelectorAll(".item").length; i++){
+//     for (var i = 0; i < document.querySelectorAll(".item").length; i++){
 
-    document.querySelectorAll(".item")[i].addEventListener("click", function(){
+//     document.querySelectorAll(".item")[i].addEventListener("click", function(){
         
-         this.classList.add("pressed");
-         var thiss = this;
-         setTimeout(function(){
-            thiss.classList.remove("pressed");
-         }, 100);
+//          this.classList.add("pressed");
+//          var thiss = this;
+//          setTimeout(function(){
+//             thiss.classList.remove("pressed");
+//          }, 100);
         
-         SoundRelatedToEach(this.id);
+//          SoundRelatedToEach(this.id);
         
-    });
+//     });
+// }
+// }
+
+function RandomNumber(){
+  return (Math.floor(Math.random() * 4));
 }
-}
 
-ClickResponse();
+function ComputerRandomMove(RandomNum){
+     switch (RandomNum) {
+    case 0:
+        
+        var greenBox = document.querySelector("#green");
+        greenBox.classList.add("pressed");
+        setTimeout(function(){
+            greenBox.classList.remove("pressed");
+            }, 100);
+            SoundRelatedToEach("green");
+      break;
+
+    case 1:
+         SoundRelatedToEach("red");
+        var redBox = document.querySelector("#red");
+        redBox.classList.add("pressed");
+        setTimeout(function(){
+            redBox.classList.remove("pressed");
+            }, 100);       
+      break;
+
+    case 2:
+        SoundRelatedToEach("yellow");
+        var yellowBox = document.querySelector("#yellow");
+        yellowBox.classList.add("pressed");
+        setTimeout(function(){
+            yellowBox.classList.remove("pressed");
+            }, 100);
+      break;
+
+    case 3:
+        SoundRelatedToEach("purple");
+        var purpleBox = document.querySelector("#purple");
+        purpleBox.classList.add("pressed");
+        setTimeout(function(){
+            purpleBox.classList.remove("pressed");
+            }, 100);
+      break;
+  
+    default:
+      console.log(RandomNum);
+      break;
+  }
+}
+  
+  ComputerRandomMove(RandomNumber());
+
+
 
 
 
