f962157b83283d477b005816876b5f0b3917b3fa
Mobina <mobinayaghoobi55@gmail.com>
2026-07-09 05:11:28 -0700

    recording moves, levelup and gameOver

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function SoundRelatedToEach(BoxColor){
+function soundRelatedToEach(BoxColor){
   switch (BoxColor) {
     case "green":
        var greenSound = new Audio("./sounds/muffled-button-click-sound.mp3");
@@ -26,41 +26,46 @@ function SoundRelatedToEach(BoxColor){
   }
 }
 
+function displayRelatedToEach(Box){
 
-
-
-// function ClickResponse(){
-
-//     for (var i = 0; i < document.querySelectorAll(".item").length; i++){
-
-//     document.querySelectorAll(".item")[i].addEventListener("click", function(){
-        
-//          this.classList.add("pressed");
-//          var thiss = this;
-//          setTimeout(function(){
-//             thiss.classList.remove("pressed");
-//          }, 100);
-        
-//          SoundRelatedToEach(this.id);
-        
-//     });
-// }
-// }
-
-function RandomNumber(){
-  return (Math.floor(Math.random() * 4));
+  Box.classList.add("pressed");
+         var boxx = Box;
+         setTimeout(function(){
+            boxx.classList.remove("pressed");
+         }, 100);
 }
 
-function ComputerRandomMove(RandomNum){
+function userClick(){
+
+    for (var i = 0; i < document.querySelectorAll(".item").length; i++){
+
+    document.querySelectorAll(".item")[i].addEventListener("click", function(){
+        
+         
+         displayRelatedToEach(this);
+         soundRelatedToEach(this.id);
+         return (this.id);
+        
+    });
+}
+}
+
+
+
+function randomNumber(){
+  return Math.floor(Math.random() * 4);
+}
+
+function computerRandomMove(RandomNum){
      switch (RandomNum) {
     case 0:
-        
+        SoundRelatedToEach("green");
         var greenBox = document.querySelector("#green");
         greenBox.classList.add("pressed");
         setTimeout(function(){
             greenBox.classList.remove("pressed");
             }, 100);
-            SoundRelatedToEach("green");
+            return "green";
       break;
 
     case 1:
@@ -69,7 +74,8 @@ function ComputerRandomMove(RandomNum){
         redBox.classList.add("pressed");
         setTimeout(function(){
             redBox.classList.remove("pressed");
-            }, 100);       
+            }, 100); 
+            return"red";      
       break;
 
     case 2:
@@ -79,6 +85,7 @@ function ComputerRandomMove(RandomNum){
         setTimeout(function(){
             yellowBox.classList.remove("pressed");
             }, 100);
+            return "yellow";
       break;
 
     case 3:
@@ -88,6 +95,7 @@ function ComputerRandomMove(RandomNum){
         setTimeout(function(){
             purpleBox.classList.remove("pressed");
             }, 100);
+            return "purple";
       break;
   
     default:
@@ -96,7 +104,49 @@ function ComputerRandomMove(RandomNum){
   }
 }
   
-  ComputerRandomMove(RandomNumber());
+  // computerRandomMove(randomNumber());
+
+
+
+function computerMoveRecord(){
+   var computerBoxes = [];
+   computerBox.push(computerRandomMove(randomNumber()));
+   return computerBoxes;
+  }
+ 
+function userMoveRecord(){
+  var userBoxes = [];
+  userBoxes.push(userClick());
+  return userBoxes;
+}
+
+function levelUp(){
+  level = 0;
+  do{
+    level++;
+    document.querySelector("h1").innerHTML = "level" + String(level);
+    for(var i = 0; i < level; i++){
+      computerMoveRecord();
+    }
+    userMoveRecord();
+   
+  }while(computerMoveRecord() === userMoveRecord());
+}
+
+function gameOver(){
+  // if(computerMoveRecord() !== userMoveRecord()){
+    document.querySelector("h1").innerHTML = "Game Over, Press  Any Key to Restart";
+    document.body.style.backgroundColor = "red";
+    setTimeout(function(){
+      document.body.style.backgroundColor = "#001233";
+    }, 200);
+    var gameOverSound = new Audio("./sounds/poppop.ai - game over audio");
+    gameOverSound.play();
+
+  }
+// }
+
+gameOver();
 
 
 
