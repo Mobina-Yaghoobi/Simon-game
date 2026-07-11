17e861467b673211c476cfd6b306fd402ca489ad
Mobina <mobinayaghoobi55@gmail.com>
2026-07-11 11:00:56 -0700

    getting the game together

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ function displayRelatedToEach(Box){
          }, 100);
 }
 
+
+ var userBoxes = [];
+function userMoveRecord(theId){
+  userBoxes.push(theId);
+  return userBoxes;
+}
+
+
 function userClick(){
 
     for (var i = 0; i < document.querySelectorAll(".item").length; i++){
@@ -44,8 +52,8 @@ function userClick(){
          
          displayRelatedToEach(this);
          soundRelatedToEach(this.id);
-         return (this.id);
-        
+        userMoveRecord(this.id);
+        checkUserMove();
     });
 }
 }
@@ -104,7 +112,6 @@ function computerRandomMove(RandomNum){
   }
 }
   
-  // computerRandomMove(randomNumber());
 
 
 var computerBoxes = [];
@@ -113,55 +120,84 @@ function computerMoveRecord(){
    return computerBoxes;
   }
 
+function computerMoveFromScratch(Level){
+for(var i = 0 ; i < Level; i++){
+  setTimeout(function(){
+    switch(computerBoxes[i]){
+    case "green":
+      computerRandomMove(0);
+    break;
 
- var computerBoxes = [];
-function userMoveRecord(){
-  userBoxes.push(userClick());
-  return userBoxes;
+    case "red":
+      computerRandomMove(1);
+    break; 
+
+    case "yellow":
+      computerRandomMove(2);
+    break;
+
+    case "purple":
+      computerRandomMove(3);
+    break;   
+    default: 
+    console.log(computerBoxes[i]);
+   
+  }
+  }, 1000);
+  
 }
+}
+ 
+
 
 function gameOver(){
-   if(computerMoveRecord() !== userMoveRecord()){
+  
     document.querySelector("h1").innerHTML = "Game Over, Press  Any Key to Restart";
     document.body.style.backgroundColor = "red";
     setTimeout(function(){
       document.body.style.backgroundColor = "#001233";
     }, 200);
-    var gameOverSound = new Audio("./sounds/poppop.ai - game over audio");
+    var gameOverSound = new Audio("./sounds/poppop.ai - game over audio.mp3");
     gameOverSound.play();
 
-  }
 }
 
-function Game(){
-  document.addEventListener("click",function(){
-    var level = 0;
-     do{
-    level++;
-    document.querySelector("h1").innerHTML = "level" + String(level);
-    for(var i = 0; i < level; i++){
-      computerRandomMove(randomNumber());
-    }
-    userClick();
-   
-  }while(comparison(computerMoveRecord(), userMoveRecord()) == true);
-  });
-  gameOver();
-}
-
-function comparison(computerMove, userMove){
+function totalComparison(computerMove, userMove){
 
 return (JSON.stringify(computerMove) === JSON.stringify(userMove));
 }
 
+function checkUserMove(){
+      computerLastElement = computerBoxes[computerBoxes.length - 1];
+      userLastElement = userBoxes[userBoxes.length - 1];
 
- Game();
+    if(computerLastElement !== userLastElement)
+      {
+          return false;
+      }
+      else{
+        true;
+      }
+    
+  }
+ 
 
 
-  
+var level = 0; 
+function Game(){
+
+  document.addEventListener("click",function(){
+     level++;
+    document.querySelector("h1").innerHTML = "level" + String(level);
+    computerMoveRecord();
+    computerMoveFromScratch(level);
+
+  });
+}
 
 
-
+userClick();
+Game();
 
 
 
